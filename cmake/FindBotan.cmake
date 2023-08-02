8bd2917f87ce31ff7fa2b6932f3e991ac8036df9
shierru <shierru.dev@gmail.com>
2023-08-02 23:30:24 +0300

    Fixed build error on Linux

--- cmake/FindBotan.cmake
+++ cmake/FindBotan.cmake
@@ -3,6 +3,12 @@
 
 cmake_minimum_required(VERSION 3.19)
 
+# Header
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(CMAKE_CXX_FLAGS "-m32")
+endif()
+
+# Includes
 include(FetchContent)
 include(CMakePrintHelpers)
 
